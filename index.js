94f8de9594fd52e6efa5d2c542f53aeef8a23a15
Tanat <tanat8511@gmail.com>
2023-02-27 16:51:37 +0600

    fix: cors added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import * as dotenv from "dotenv";
 dotenv.config()
 import express from 'express'
 import userRouter from "./routes/userRouter.js";
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 4444
@@ -9,6 +10,7 @@ const port = process.env.PORT || 4444
 
 app.use(express.json())
 app.use('/api', userRouter)
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send( )
